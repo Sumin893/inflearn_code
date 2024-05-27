45099f85ba6bf9d8f94a92eb4a618b7d845818d5
Sumin893 <ea0307@naver.com>
2024-05-28 02:12:42 +0900

    ~inflearn 5.10

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -5,48 +5,84 @@ import { useState } from "react";
 
 const Register=() => {
 
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("자기소개")
+  const [input, setInput] = useState({
+    name : "",
+    birth : "",
+    country : "",
+    bio : "",
+  });
 
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  }
+  //위에 const처럼 아래 4줄을 간단히 만들 수 있음.
+  // const [name, setName] = useState("이름");
+  // const [birth, setBirth] = useState("");
+  // const [country, setCountry] = useState("");
+  // const [bio, setBio] = useState("자기소개")
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  }
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  }
+
+    //아래의 4개의 event handler도 위에 한개로 줄일 수 있음.
+  // const onChangeName = (e) => {
+  //   setInput({
+  //     ...input, //spread연산자 이걸 써야 나머지 생일,국적,자기소개의 property가 사라지지 않음.
+  //     name : e.target.value
+  //   });
+  // };
+
+  // const onChangeBirth = (e) => {
+  //   setInput({
+  //   ...input,
+  //   birth : e.target.value
+  //   });
+  // };
+
+  // const onChangeCountry = (e) => {
+  //   setInput({
+  //     ...input,
+  //     country : e.target.value
+  //     });
+  // };
+
+  // const onChangeBio = (e) => {
+  //   setInput({
+  //     ...input,
+  //     bio : e.target.value
+  //     });
+  // };
 
   return (
     <div>
       <div>
         <input 
-        value={name}
-        onChange={onChangeName}
+        name="name"
+        value={input.name}
+        onChange={onChange}
         placeholder={"이름"}
         />
       </div>
 
       <div>
         <input
-        value={birth}
-        onChange={onChangeBirth}
+        birth="birth"
+        value={input.birth}
+        onChange={onChange}
         type="date"
         />
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select 
+        name="country"
+          value={input.country} 
+          onChange={onChange}
+        >
           <option vlaue=""></option>
           <option vlaue="kr">한국</option>
           <option value="uk">영국</option>
@@ -55,9 +91,12 @@ const Register=() => {
       </div>
 
       <div>
-        <textarea value={bio} onchange={onChangeBio}/>
+        <textarea 
+          name="bio"
+          value={input.bio}
+          onChange={onChange}
+        />
       </div>
-
     </div>
   );
 };
